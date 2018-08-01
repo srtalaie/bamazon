9afd195b01373c0000d2110dffcdfb9ef40c615a
srtalaie <srt4721@yahoo.com>
2018-07-31 17:10:31 -0700

    Added code to create connection to database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,12 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
-const password = require('./password');
+const myPass = require('./password.js');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: myPass.password,
+    database: 'top_songsdb'
+});
