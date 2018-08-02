50af2d657b2d8595a877d37dcce6367e4e1e209d
srtalaie <srt4721@yahoo.com>
2018-08-02 13:04:52 -0700

    Made the customer app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,5 +8,75 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: myPass.password,
-    database: 'top_songsdb'
+    database: 'bamazon'
 });
+
+function start(){
+    console.log('Please choose an item you would like to purchase:')
+    displayItems();
+
+    inquirer.prompt([
+        {
+            name: 'itemId',
+            message: 'Please enter in the product ID of the item you would like to purchase.',
+            type: 'input',
+            validate: function(name){
+                if(isNaN(name)){
+                    return 'Please choose the product ID of the item you would like to purchase.';
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            name: 'amount',
+            message: 'How many would you like to purchase?',
+            type: 'input',
+            validate: function(name){
+                if(isNaN(name)){
+                    return 'Please enter in a valid number.';
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(answer){
+        fulfillOrder(answer.itemId, answer.amount);
+    })
+}
+
+
+start();
+
+
+//UTILS
+function displayItems(){
+    connection.query('SELECT * FROM products', function(err, res){
+        if (err) throw err;
+
+        res.forEach(item => {
+            console.log(`
+                ID: ${item.item_id}
+                Product: ${item.product_name}
+                Price: $${item.price}
+                Department: ${item.department_name}
+                //////////////////////////////////////////////////////////
+            `)
+        });
+    })
+}
+
+function fulfillOrder(id, amount){
+    let query = `UPDATE products SET stock_quantity = stock_quantity - ${amount} WHERE item_id = ${id} and stock_quantity > ${amount}`;
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        //Check to see if there is enough in supply
+        if (res.changedRows === 0){
+            console.log('Insufficient Supply.');
+            start();
+        } else {
+            console.log('Congrats! Your order of has gone through.');
+            start();
+        }
+    })
+}
